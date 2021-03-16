955acf7860a38c60a19692660005da440da33ee1
Lisa Sau <lisasau85@gmail.com>
2021-03-16 12:13:54 +0000

    server config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./people.json')
+const router = jsonServer.router('./db/people.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000;
 
